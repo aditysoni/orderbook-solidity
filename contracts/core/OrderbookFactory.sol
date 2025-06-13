4229ea7998dea26dab78fc9025a501bcfa5ca6e1
Aditya <adisoni1017@gmail.com>
2025-06-13 16:37:16 +0530

    added trasfer functionality

--- contracts/core/OrderbookFactory.sol
+++ contracts/core/OrderbookFactory.sol
@@ -8,13 +8,13 @@ contract OrderbookFactory {
     mapping(bytes32 => address) public marketToOrderbook ;
     address[] public allOrderbooks;
 
-    event OrderbookCreated(bytes32 indexed marketHash, address orderbook, string baseToken, string quoteToken);
+    event OrderbookCreated(bytes32 indexed marketHash, address orderbook, address baseToken, address quoteToken);
 
-    function createOrderbook(string memory baseToken, string memory quoteToken) external returns (address) {
+    function createOrderbook(address baseToken, address quoteToken) external returns (address) {
         bytes32 marketHash = keccak256(abi.encodePacked(baseToken, quoteToken));
         require(marketToOrderbook[marketHash] == address(0), "Orderbook already exists");
 
-        Orderbook orderbook = new Orderbook();
+        Orderbook orderbook = new Orderbook(marketHash, quoteToken, baseToken);
         marketToOrderbook[marketHash] = address(orderbook);
         allOrderbooks.push(address(orderbook));
 
